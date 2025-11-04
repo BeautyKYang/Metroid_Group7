71ca58aec01686946f8c95b353153114ac039a27
LjC0mp00ts <lanajadedalman@gmail.com>
2025-11-04 11:56:22 -0800

    finished health and added movement

--- Assets/Scenes/LanajadeWork/Scripts/Health.cs
+++ Assets/Scenes/LanajadeWork/Scripts/Health.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     public int life = 99;
 
-    [Header("MaterialBlinkDMG")]
-    public Material newMaterial;
-    private MeshRenderer meshRenderer;
-
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        // Get the MeshRenderer component attached to this GameObject
-        meshRenderer = GetComponent<MeshRenderer>();
+        if (other.gameObject.tag == "RegularEnemy")
+        {
+            life -= 15;
+            StartCoroutine(Blinking());
+            print("you lost 15 life");
+        }
+
+        if (other.gameObject.tag == "HardEnemy")
+        {
+            life -= 35;
+            StartCoroutine(Blinking());
+            print("Hard enemy destroys u");
+        }
+    }
+
+    public IEnumerator Blinking()
+    {
+        for (int index = 0; index < 10; index++)
+
+        {
+            if (index % 2 == 0)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().enabled = true;
+            }
+
+            yield return new WaitForSeconds(0.25f);
+
+        }
+        GetComponent<MeshRenderer>().enabled = true;
+
+        
     }
 }
