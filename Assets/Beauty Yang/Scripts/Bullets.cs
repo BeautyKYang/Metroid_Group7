94d13427ba1650f69db2b11a28403d93c51b0a8a
BeautyKYang <beautyyang00@gmail.com>
2025-11-05 18:58:09 -0800

    Enemy Script (In Progress)

--- Assets/Beauty Yang/Scripts/Bullets.cs
+++ Assets/Beauty Yang/Scripts/Bullets.cs
@@ -10,24 +10,4 @@ public class Bullets : MonoBehaviour
     public GameObject regularBulletsPrefab;
 
     public int RegularBullets = 1;
-
-    /*
-    public void DecreaseLives()
-    {
-        //Subtract one life
-        lives--;
-
-        if (lives == 2) //1 life lost - disable first bucket
-            enemy.SetActive(false);
-        else if (lives == 1) //2 life lost - disable second bucket
-            enemy.SetActive(false);
-        else //3 life lost - disable third bucket
-        {
-            enemy.SetActive(false);
-
-            //Load game over screen
-            SceneManager.LoadScene(2); //Or use: StartCoroutine(WaitToLoadGameOver(2f));
-        }
-    }
-    */
 }
--- Assets/Beauty Yang/Scripts/RegularEnemy.cs
+++ Assets/Beauty Yang/Scripts/RegularEnemy.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Cinemachine.CinemachineFreeLook;
 
 public class RegularEnemy : MonoBehaviour
 {
+    public GameObject enemyPrefab;
+    public int lives = 1;
+
+    public Transform leftPoint;
+    public Transform rightPoint;
+    public float speed = 5;
+
+    private Vector3 direction;
+    private Vector3 startLeftpos;
+    private Vector3 startRightpos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = Vector3.left;
+        startLeftpos = leftPoint.position;
+        startRightpos = rightPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+
+        //Check if enemy has reached the left point to turn around
+        if (transform.position.x <= startLeftpos.x) // other way to code: (direction == Vector3.left && transform.position.x <= startLeftpos.x) //== is comparing the values of the variables
+        {
+            direction = Vector3.right;
+        }
+        else if (transform.position.x >= startRightpos.x) //Check if enemy has reached the right point to turn around
+        {
+            direction = Vector3.left;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If bullet hits the enemy the enemy dies
+        if (other.GetComponent<Bullets>())
+        {
+            lives--; //Enemy loses a life
+            Destroy(other.gameObject);
+        }
     }
 }
