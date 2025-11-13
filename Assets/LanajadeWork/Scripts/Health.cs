ce326d1d13155a1cf59d32024446dd4f52a7e8ec
LjC0mp00ts <lanajadedalman@gmail.com>
2025-11-12 23:20:35 -0800

    Lj practice
    health fixes

--- Assets/LanajadeWork/Scripts/Health.cs
+++ Assets/LanajadeWork/Scripts/Health.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+/*
+Lanajade Dalman
+11/12/25
+manages health and health kits
+*/
 public class Health : MonoBehaviour
 {
     public int life = 99;
     public bool isBlinking = false;
     public int amountHealed = 20;
+
+    /// <summary>
+    /// if player dies
+    /// </summary>
+    private void Update()
+    {
+        if (life == 0)
+        {
+            //sends to end game
+            SceneManager.LoadScene(2);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -45,24 +62,46 @@ public class Health : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// regular health pack functions
+    /// </summary>
     public void RegHeal()
     {
         if (life < 99)
         {
+            //life gets healed
             life += amountHealed;
+            print("you healed " + amountHealed + " life!");
+
+            //makes sure that life does not exceed 99
+            if (life > 99)
+            {
+                life = 99;
+            }
+            
+            print("Life: " + life);
         }
-        else
+        //life does not exceed 99
+        else if (life == 99)
         {
             print("You have full health!");
         }
     }
 
+    /// <summary>
+    /// full health pack
+    /// </summary>
     public void FullHeal()
     {
+        //max life adds 100 and life is fully healed
         life = 99;
         life += 100;
     }
 
+    /// <summary>
+    /// blinks when damaged
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator Blinking()
     {
         for (int index = 0; index < 10; index++)
